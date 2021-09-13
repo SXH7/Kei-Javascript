7915c7d0970290be0ac0868ad25219cb91061bd2
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2021-09-13 20:42:26 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+//const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
+client.once("ready", ()=>{
+    console.log("Bot is online!")
+});
+
+client.login('TOKEN');
