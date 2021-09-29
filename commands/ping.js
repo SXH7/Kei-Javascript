a04063fa535b5c1afa6f7790b59ca628f8cb9e58
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2021-09-29 18:02:15 +0530

    Delete ping.js

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'ping command',
-    execute(message, args){
-        message.channel.send(`🏓Latency is ${Date.now()-message.createdTimestamp}ms.`);
-    }
-}
